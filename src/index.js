5bc9e7bc69f41d91e541679d58152f06b9559dc9
Ascend <anandu.ug20@nsut.ac.in>
2023-07-27 14:43:10 +0530

    Keeper App 3

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
 
 //CHALLENGE:
 //1. Implement the add note functionality.
